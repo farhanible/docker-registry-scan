220eb6b2dcf9118c3b192372cd9d5dcd4dab6568
farhanible <fahmed@2u.com>
2016-07-29 18:17:56 -0400

    Update dockerscan.py

--- dockerscan.py
+++ dockerscan.py
@@ -4,6 +4,7 @@ with open('docker_shodan.csv', 'rb') as csvfile:
 	spamreader = csv.DictReader(csvfile)
 	for row in spamreader:
 		try:
+			#Works for v2. Didn't find that many open v1 registries so /shrug
 			r = requests.get('http://' + row['IP'] + ':5000/v2/_catalog')
 			if r.status_code == 400:
 				r = requests.get('https://' + thing + ':5000/v2/_catalog', verify=False)
