17e8a91b960fd1ee857bbed2ffe7e43f34183eaa
farhanible <fahmed@2u.com>
2016-07-30 17:54:24 -0400

    Update dockerscan.py

--- dockerscan.py
+++ dockerscan.py
@@ -7,7 +7,8 @@ with open('docker_shodan.csv', 'rb') as csvfile:
 			#Works for v2. Didn't find that many open v1 registries so /shrug
 			r = requests.get('http://' + row['IP'] + ':5000/v2/_catalog')
 			if r.status_code == 400:
-				r = requests.get('https://' + thing + ':5000/v2/_catalog', verify=False)
+				#Verify false because we may be hitting open registries with IPs instead of fqdns
+				r = requests.get('https://' + row['IP'] + ':5000/v2/_catalog', verify=False)
 			print row['IP'] + ' = ' + r.text
 		except Exception, e:
 #      		print e
